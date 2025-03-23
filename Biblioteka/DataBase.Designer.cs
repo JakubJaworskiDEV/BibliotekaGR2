bc89d29dac96925d5580c0d2d561a37accbb09ea
ArturKupisz <UL0284663@edu.uni.lodz.pl>
2025-03-23 12:28:24 +0100

    zmiany

--- Biblioteka/DataBase.Designer.cs
+++ Biblioteka/DataBase.Designer.cs
@@ -29,33 +29,84 @@
         private void InitializeComponent()
         {
             dataGridViewUser = new DataGridView();
+            cmbSearchCriteria = new ComboBox();
+            txtSearch = new TextBox();
+            btnSearchUser = new Button();
+            btnResetSearch = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridViewUser).BeginInit();
             SuspendLayout();
             // 
             // dataGridViewUser
             // 
             dataGridViewUser.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewUser.Location = new Point(21, 51);
+            dataGridViewUser.Location = new Point(11, 88);
+            dataGridViewUser.Margin = new Padding(2, 2, 2, 2);
             dataGridViewUser.Name = "dataGridViewUser";
             dataGridViewUser.RowHeadersWidth = 62;
-            dataGridViewUser.Size = new Size(1121, 225);
+            dataGridViewUser.Size = new Size(834, 403);
             dataGridViewUser.TabIndex = 0;
+            // 
+            // cmbSearchCriteria
+            // 
+            cmbSearchCriteria.FormattingEnabled = true;
+            cmbSearchCriteria.Items.AddRange(new object[] { "Nazwisko", "PESEL", "Login", "Email", "Telefon" });
+            cmbSearchCriteria.Location = new Point(337, 36);
+            cmbSearchCriteria.Name = "cmbSearchCriteria";
+            cmbSearchCriteria.Size = new Size(138, 23);
+            cmbSearchCriteria.TabIndex = 1;
+            // 
+            // txtSearch
+            // 
+            txtSearch.Location = new Point(481, 36);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(207, 23);
+            txtSearch.TabIndex = 2;
+            // 
+            // btnSearchUser
+            // 
+            btnSearchUser.Location = new Point(708, 31);
+            btnSearchUser.Name = "btnSearchUser";
+            btnSearchUser.Size = new Size(118, 23);
+            btnSearchUser.TabIndex = 3;
+            btnSearchUser.Text = "Wyszukaj";
+            btnSearchUser.UseVisualStyleBackColor = true;
+            btnSearchUser.Click += btnSearchUser_Click;
+            // 
+            // btnResetSearch
+            // 
+            btnResetSearch.Location = new Point(709, 60);
+            btnResetSearch.Name = "btnResetSearch";
+            btnResetSearch.Size = new Size(117, 23);
+            btnResetSearch.TabIndex = 4;
+            btnResetSearch.Text = "Reset";
+            btnResetSearch.UseVisualStyleBackColor = true;
+            btnResetSearch.Click += btnResetSearch_Click;
             // 
             // DataBase
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1148, 450);
+            ClientSize = new Size(856, 502);
+            Controls.Add(btnResetSearch);
+            Controls.Add(btnSearchUser);
+            Controls.Add(txtSearch);
+            Controls.Add(cmbSearchCriteria);
             Controls.Add(dataGridViewUser);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "DataBase";
             Text = "DataBase";
             Load += DataBase_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridViewUser).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dataGridViewUser;
+        private ComboBox cmbSearchCriteria;
+        private TextBox txtSearch;
+        private Button btnSearchUser;
+        private Button btnResetSearch;
     }
 }
--- Biblioteka/DataBase.cs
+++ Biblioteka/DataBase.cs
@@ -28,7 +28,7 @@ namespace Biblioteka
                 using (SQLiteConnection connection = new SQLiteConnection(connectionString))
                 {
                     connection.Open();
-                    
+
                     MessageBox.Show("Połączono z bazą danych!", "Sukces", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
                     LoadUsers(); // Załaduj użytkowników do DataGridView
@@ -40,7 +40,7 @@ namespace Biblioteka
             }
         }
 
-        
+
 
         private void LoadUsers()
         {
@@ -62,6 +62,77 @@ namespace Biblioteka
             catch (Exception ex)
             {
                 MessageBox.Show($"Błąd podczas pobierania danych: {ex.Message}", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSearchUser_Click(object sender, EventArgs e)
+        {
+            if (cmbSearchCriteria.SelectedItem != null)
+            {
+                if (string.IsNullOrWhiteSpace(txtSearch.Text))
+                {
+                    MessageBox.Show("Proszę wpisać wartość do wyszukiwania.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                string selectedCriteria = cmbSearchCriteria.SelectedItem.ToString();
+                SearchUsers(txtSearch.Text, selectedCriteria);
+            }
+            else
+            {
+                MessageBox.Show("Proszę wybrać kryterium wyszukiwania.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnResetSearch_Click(object sender, EventArgs e)
+        {
+            txtSearch.Text = "";
+            cmbSearchCriteria.SelectedIndex = -1;
+            LoadUsers();
+        }
+
+        private void SearchUsers(string searchText, string criteria)
+        {
+            try
+            {
+                string column;
+                switch (criteria)
+                {
+                    case "Nazwisko":
+                        column = "Nazwisko";
+                        break;
+                    case "PESEL":
+                        column = "PESEL";
+                        break;
+                    case "Login":
+                        column = "Login";
+                        break;
+                    case "Email":
+                        column = "Email";
+                        break;
+                    case "Telefon":
+                        column = "Telefon";
+                        break;
+                    default:
+                        MessageBox.Show("Nieprawidłowe kryterium wyszukiwania.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                }
+
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = $"SELECT * FROM Uzytkownik WHERE {column} LIKE @searchText";
+                    using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(query, connection))
+                    {
+                        adapter.SelectCommand.Parameters.AddWithValue("@searchText", "%" + searchText + "%");
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+                        dataGridViewUser.DataSource = dt;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Błąd podczas wyszukiwania: {ex.Message}", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
