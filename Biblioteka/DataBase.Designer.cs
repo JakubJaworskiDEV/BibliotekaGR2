e1c3ee4a2bc82ba798f028c2f4ad3b7e21744e09
jakub <jakub@LAPTOP-F6UP0HL1>
2025-03-23 11:25:57 +0100

    Dodaj pliki projektów.

---
+++ Biblioteka/DataBase.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Biblioteka
+{
+    partial class DataBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridViewUser = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewUser).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridViewUser
+            // 
+            dataGridViewUser.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewUser.Location = new Point(21, 51);
+            dataGridViewUser.Name = "dataGridViewUser";
+            dataGridViewUser.RowHeadersWidth = 62;
+            dataGridViewUser.Size = new Size(1121, 225);
+            dataGridViewUser.TabIndex = 0;
+            // 
+            // DataBase
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1148, 450);
+            Controls.Add(dataGridViewUser);
+            Name = "DataBase";
+            Text = "DataBase";
+            Load += DataBase_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridViewUser).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridViewUser;
+    }
+}
---
+++ Biblioteka/DataBase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+namespace Biblioteka
+{
+    public partial class DataBase : Form
+    {
+        private string dbPath = @"..\..\..\..\BazaDanychProjekt.db";
+        private string connectionString;
+
+        public DataBase()
+        {
+            InitializeComponent();
+            connectionString = $"Data Source={dbPath};Version=3;";
+        }
+        private void DataBase_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!System.IO.File.Exists(dbPath))
+                {
+                    SQLiteConnection.CreateFile(dbPath);
+                    MessageBox.Show("Utworzono nową bazę danych.");
+                }
+
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+                    
+                    MessageBox.Show("Połączono z bazą danych!", "Sukces", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    LoadUsers(); // Załaduj użytkowników do DataGridView
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Błąd połączenia z bazą: {ex.Message}", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        
+
+        private void LoadUsers()
+        {
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM Uzytkownik";
+
+                    using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(query, connection))
+                    {
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+                        dataGridViewUser.DataSource = dt; // Wyświetlenie danych w DataGridView
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Błąd podczas pobierania danych: {ex.Message}", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
