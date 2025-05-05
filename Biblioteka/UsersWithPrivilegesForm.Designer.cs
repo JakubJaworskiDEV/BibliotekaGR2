8d0dbf17eff8250d8159c939c0fa6def649926dd
ArturKupisz <UL0284663@edu.uni.lodz.pl>
2025-05-05 18:54:09 +0200

    Wyszukiwarka po Kryteriach w Wyszukiwaniu po uprawnieniach

--- Biblioteka/UsersWithPrivilegesForm.Designer.cs
+++ Biblioteka/UsersWithPrivilegesForm.Designer.cs
@@ -31,6 +31,14 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UsersWithPrivilegesForm));
             dgvUsers = new DataGridView();
             btnUsrWPrivFormClose = new Button();
+            btnSearchWithPrivileges = new Button();
+            chkNamePriv = new CheckBox();
+            chkSurnamePriv = new CheckBox();
+            chkLoginPriv = new CheckBox();
+            txtNamePriv = new TextBox();
+            txtSurnamePriv = new TextBox();
+            txtLoginPriv = new TextBox();
+            btnResetPrivSearch = new Button();
             ((System.ComponentModel.ISupportInitialize)dgvUsers).BeginInit();
             SuspendLayout();
             // 
@@ -39,7 +47,7 @@
             dgvUsers.BackgroundColor = Color.FromArgb(255, 224, 192);
             dgvUsers.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dgvUsers.GridColor = Color.FromArgb(255, 224, 192);
-            dgvUsers.Location = new Point(59, 72);
+            dgvUsers.Location = new Point(59, 120);
             dgvUsers.Name = "dgvUsers";
             dgvUsers.Size = new Size(630, 327);
             dgvUsers.TabIndex = 0;
@@ -49,7 +57,7 @@
             btnUsrWPrivFormClose.BackColor = Color.FromArgb(64, 64, 64);
             btnUsrWPrivFormClose.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
             btnUsrWPrivFormClose.ForeColor = SystemColors.Control;
-            btnUsrWPrivFormClose.Location = new Point(59, 11);
+            btnUsrWPrivFormClose.Location = new Point(24, 12);
             btnUsrWPrivFormClose.Name = "btnUsrWPrivFormClose";
             btnUsrWPrivFormClose.Size = new Size(84, 33);
             btnUsrWPrivFormClose.TabIndex = 1;
@@ -57,23 +65,118 @@
             btnUsrWPrivFormClose.UseVisualStyleBackColor = false;
             btnUsrWPrivFormClose.Click += btnUsrWPrivFormClose_Click;
             // 
+            // btnSearchWithPrivileges
+            // 
+            btnSearchWithPrivileges.Location = new Point(653, 18);
+            btnSearchWithPrivileges.Name = "btnSearchWithPrivileges";
+            btnSearchWithPrivileges.Size = new Size(135, 27);
+            btnSearchWithPrivileges.TabIndex = 2;
+            btnSearchWithPrivileges.Text = "Wyszukaj";
+            btnSearchWithPrivileges.UseVisualStyleBackColor = true;
+            btnSearchWithPrivileges.Click += btnSearchWithPrivileges_Click;
+            // 
+            // chkNamePriv
+            // 
+            chkNamePriv.AutoSize = true;
+            chkNamePriv.Location = new Point(209, 18);
+            chkNamePriv.Name = "chkNamePriv";
+            chkNamePriv.Size = new Size(49, 19);
+            chkNamePriv.TabIndex = 3;
+            chkNamePriv.Text = "Imię";
+            chkNamePriv.UseVisualStyleBackColor = true;
+            chkNamePriv.CheckedChanged += chkNamePriv_CheckedChanged;
+            // 
+            // chkSurnamePriv
+            // 
+            chkSurnamePriv.AutoSize = true;
+            chkSurnamePriv.Location = new Point(209, 43);
+            chkSurnamePriv.Name = "chkSurnamePriv";
+            chkSurnamePriv.Size = new Size(76, 19);
+            chkSurnamePriv.TabIndex = 4;
+            chkSurnamePriv.Text = "Nazwisko";
+            chkSurnamePriv.UseVisualStyleBackColor = true;
+            chkSurnamePriv.CheckedChanged += chkSurnamePriv_CheckedChanged;
+            // 
+            // chkLoginPriv
+            // 
+            chkLoginPriv.AutoSize = true;
+            chkLoginPriv.Location = new Point(209, 68);
+            chkLoginPriv.Name = "chkLoginPriv";
+            chkLoginPriv.Size = new Size(56, 19);
+            chkLoginPriv.TabIndex = 5;
+            chkLoginPriv.Text = "Login";
+            chkLoginPriv.UseVisualStyleBackColor = true;
+            chkLoginPriv.CheckedChanged += chkLoginPriv_CheckedChanged;
+            // 
+            // txtNamePriv
+            // 
+            txtNamePriv.Location = new Point(302, 14);
+            txtNamePriv.Name = "txtNamePriv";
+            txtNamePriv.Size = new Size(195, 23);
+            txtNamePriv.TabIndex = 6;
+            txtNamePriv.Visible = false;
+            txtNamePriv.TextChanged += textBox1_TextChanged;
+            // 
+            // txtSurnamePriv
+            // 
+            txtSurnamePriv.Location = new Point(302, 41);
+            txtSurnamePriv.Name = "txtSurnamePriv";
+            txtSurnamePriv.Size = new Size(195, 23);
+            txtSurnamePriv.TabIndex = 7;
+            txtSurnamePriv.Visible = false;
+            // 
+            // txtLoginPriv
+            // 
+            txtLoginPriv.Location = new Point(302, 66);
+            txtLoginPriv.Name = "txtLoginPriv";
+            txtLoginPriv.Size = new Size(195, 23);
+            txtLoginPriv.TabIndex = 8;
+            txtLoginPriv.Visible = false;
+            // 
+            // btnResetPrivSearch
+            // 
+            btnResetPrivSearch.Location = new Point(651, 54);
+            btnResetPrivSearch.Name = "btnResetPrivSearch";
+            btnResetPrivSearch.Size = new Size(137, 33);
+            btnResetPrivSearch.TabIndex = 9;
+            btnResetPrivSearch.Text = "Reset";
+            btnResetPrivSearch.UseVisualStyleBackColor = true;
+            btnResetPrivSearch.Click += btnResetPrivSearch_Click;
+            // 
             // UsersWithPrivilegesForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             ClientSize = new Size(800, 450);
+            Controls.Add(btnResetPrivSearch);
+            Controls.Add(txtLoginPriv);
+            Controls.Add(txtSurnamePriv);
+            Controls.Add(txtNamePriv);
+            Controls.Add(chkLoginPriv);
+            Controls.Add(chkSurnamePriv);
+            Controls.Add(chkNamePriv);
+            Controls.Add(btnSearchWithPrivileges);
             Controls.Add(btnUsrWPrivFormClose);
             Controls.Add(dgvUsers);
             Name = "UsersWithPrivilegesForm";
             Text = "UsersWithPrivilegesForm";
             ((System.ComponentModel.ISupportInitialize)dgvUsers).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dgvUsers;
         private Button btnUsrWPrivFormClose;
+        private Button btnSearchWithPrivileges;
+        private CheckBox chkNamePriv;
+        private CheckBox chkSurnamePriv;
+        private CheckBox chkLoginPriv;
+        private TextBox txtNamePriv;
+        private TextBox txtSurnamePriv;
+        private TextBox txtLoginPriv;
+        private Button btnResetPrivSearch;
     }
 }
--- Biblioteka/UsersWithPrivilegesForm.cs
+++ Biblioteka/UsersWithPrivilegesForm.cs
@@ -23,7 +23,7 @@ namespace Biblioteka
             LoadUsersWithPrivileges();
         }
 
-        private void LoadUsersWithPrivileges()
+        private void LoadUsersWithPrivileges(string additionalCondition = "", List<SQLiteParameter> parameters = null)
         {
             try
             {
@@ -37,15 +37,21 @@ namespace Biblioteka
                         FROM Uzytkownik u
                         JOIN Uprawnienia_Uzytkownik uu ON u.Uzytkownik_id = uu.Uzytkownik_ID
                         JOIN Uprawnienia up ON uu.Uprawnienia_ID = up.Uprawnienia_ID
-                        WHERE up.Nazwa IN ({privilegeSubquery}) AND u.Status_akt = 1
+                        WHERE up.Nazwa IN ({privilegeSubquery}) AND u.Status_akt = 1 {additionalCondition}
                         GROUP BY u.Uzytkownik_id
                         HAVING COUNT(DISTINCT up.Nazwa) = {privileges.Count};";
 
-                    using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(query, connection))
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
                     {
-                        DataTable dt = new DataTable();
-                        adapter.Fill(dt);
-                        dgvUsers.DataSource = dt;
+                        if (parameters != null)
+                            command.Parameters.AddRange(parameters.ToArray());
+
+                        using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(command))
+                        {
+                            DataTable dt = new DataTable();
+                            adapter.Fill(dt);
+                            dgvUsers.DataSource = dt;
+                        }
                     }
                 }
             }
@@ -58,6 +64,65 @@ namespace Biblioteka
         private void btnUsrWPrivFormClose_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void btnSearchWithPrivileges_Click(object sender, EventArgs e)
+        {
+            List<string> conditions = new List<string>();
+            List<SQLiteParameter> parameters = new List<SQLiteParameter>();
+
+            if (chkNamePriv.Checked && !string.IsNullOrWhiteSpace(txtNamePriv.Text))
+            {
+                conditions.Add("AND u.Imie LIKE @name");
+                parameters.Add(new SQLiteParameter("@name", "%" + txtNamePriv.Text + "%"));
+            }
+            if (chkSurnamePriv.Checked && !string.IsNullOrWhiteSpace(txtSurnamePriv.Text))
+            {
+                conditions.Add("AND u.Nazwisko LIKE @surname");
+                parameters.Add(new SQLiteParameter("@surname", "%" + txtSurnamePriv.Text + "%"));
+            }
+            if (chkLoginPriv.Checked && !string.IsNullOrWhiteSpace(txtLoginPriv.Text))
+            {
+                conditions.Add("AND u.Login LIKE @login");
+                parameters.Add(new SQLiteParameter("@login", "%" + txtLoginPriv.Text + "%"));
+            }
+
+            string conditionClause = string.Join(" ", conditions);
+            LoadUsersWithPrivileges(conditionClause, parameters);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkNamePriv_CheckedChanged(object sender, EventArgs e)
+        {
+            txtNamePriv.Visible = chkNamePriv.Checked;
+            if (!chkNamePriv.Checked) txtNamePriv.Clear();
+        }
+
+        private void chkSurnamePriv_CheckedChanged(object sender, EventArgs e)
+        {
+            txtSurnamePriv.Visible = chkSurnamePriv.Checked;
+            if (!chkSurnamePriv.Checked) txtSurnamePriv.Clear();
+        }
+
+        private void chkLoginPriv_CheckedChanged(object sender, EventArgs e)
+        {
+            txtLoginPriv.Visible = chkLoginPriv.Checked;
+            if (!chkLoginPriv.Checked) txtLoginPriv.Clear();
+        }
+
+        private void btnResetPrivSearch_Click(object sender, EventArgs e)
+        {
+            txtNamePriv.Text = "";
+            txtSurnamePriv.Text = "";
+            txtLoginPriv.Text = "";
+            chkNamePriv.Checked = false;
+            chkSurnamePriv.Checked = false;
+            chkLoginPriv.Checked = false;
+            LoadUsersWithPrivileges();
         }
     }
 }
