2cdf594d017d74793f86aeb36e1822eadf9b8415
JakubJaworskiDEV <UL0284679@edu.uni.lodz.pl>
2025-05-10 19:47:07 +0200

    poprawki v1

---
+++ Biblioteka/ChangePasswordForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Biblioteka
+{
+    partial class ChangePasswordForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ChangePassword = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            NewPassword = new TextBox();
+            AgainNewPassword = new TextBox();
+            SuspendLayout();
+            // 
+            // ChangePassword
+            // 
+            ChangePassword.Location = new Point(288, 343);
+            ChangePassword.Name = "ChangePassword";
+            ChangePassword.Size = new Size(185, 44);
+            ChangePassword.TabIndex = 0;
+            ChangePassword.Text = "Zmień hasło";
+            ChangePassword.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(299, 110);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Podaj nowe hasło";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(299, 157);
+            label2.Name = "label2";
+            label2.Size = new Size(122, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Potwierdź nowe hasło";
+            // 
+            // NewPassword
+            // 
+            NewPassword.Location = new Point(299, 131);
+            NewPassword.Name = "NewPassword";
+            NewPassword.Size = new Size(174, 23);
+            NewPassword.TabIndex = 3;
+            // 
+            // AgainNewPassword
+            // 
+            AgainNewPassword.Location = new Point(299, 175);
+            AgainNewPassword.Name = "AgainNewPassword";
+            AgainNewPassword.Size = new Size(174, 23);
+            AgainNewPassword.TabIndex = 4;
+            // 
+            // ChangePasswordForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(AgainNewPassword);
+            Controls.Add(NewPassword);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(ChangePassword);
+            Name = "ChangePasswordForm";
+            Text = "ChangePasswordForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button ChangePassword;
+        private Label label1;
+        private Label label2;
+        private TextBox NewPassword;
+        private TextBox AgainNewPassword;
+    }
+}
--- Biblioteka/RemaindPasswordForm.cs
+++ Biblioteka/RemaindPasswordForm.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,73 @@ namespace Biblioteka
         public RemaindPasswordForm()
         {
             InitializeComponent();
+        }
+
+        private string GenerateSecurePassword(int length = 12)
+        {
+            const string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const string lower = "abcdefghijklmnopqrstuvwxyz";
+            const string digits = "0123456789";
+            const string special = "!@#$%^&*()-_=+[]{};:<>/?";
+
+            string allChars = upper + lower + digits + special;
+            StringBuilder password = new StringBuilder();
+            Random rnd = new Random();
+
+            // Dodaj przynajmniej jeden znak z każdej kategorii
+            password.Append(upper[rnd.Next(upper.Length)]);
+            password.Append(lower[rnd.Next(lower.Length)]);
+            password.Append(digits[rnd.Next(digits.Length)]);
+            password.Append(special[rnd.Next(special.Length)]);
+
+            // Dodaj pozostałe znaki
+            for (int i = password.Length; i < length; i++)
+            {
+                password.Append(allChars[rnd.Next(allChars.Length)]);
+            }
+
+            // Wymieszaj hasło
+            return Shuffle(password.ToString(), rnd);
+        }
+
+        private string Shuffle(string input, Random rnd)
+        {
+            char[] array = input.ToCharArray();
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                int j = rnd.Next(i + 1);
+                (array[i], array[j]) = (array[j], array[i]);
+            }
+            return new string(array);
+        }
+
+
+        private void SendPasswordToEmail(string recipientEmail, string newPassword)
+        {
+            string fromEmail = "twojemail@gmail.com";
+            string fromPassword = "haslo_aplikacji";
+
+            try
+            {
+                MailMessage message = new MailMessage();
+                message.From = new MailAddress(fromEmail);
+                message.To.Add(recipientEmail);
+                message.Subject = "Nowe hasło";
+                message.Body = $"Twoje nowe hasło to: {newPassword}";
+
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                smtp.Credentials = new NetworkCredential(fromEmail, fromPassword);
+                smtp.EnableSsl = true;
+                smtp.Send(message);
+
+                MessageBox.Show("Hasło zostało wysłane na e-mail.", "Sukces", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Błąd wysyłania e-maila: " + ex.Message, "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
         }
     }
 }
